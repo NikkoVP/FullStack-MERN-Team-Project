e9ae2b449ff27546e4c07e0b86e4e69a364174bd
Cyril John De Leon <deleo.cyriljohn21@gmail.com>
2023-07-29 21:36:17 +0800

    'updating design'

--- frontend/src/Component/router/router.jsx
+++ frontend/src/Component/router/router.jsx
@@ -2,6 +2,7 @@ import {createBrowserRouter} from 'react-router-dom';
 import Login from '../login/Login';
 import Register from '../register/Register';
 import HomePage from '../homepage/Homepage';
+import Todo from '../todoComponent/Todo';
 const router = createBrowserRouter([
     {
         path:'/',
@@ -20,7 +21,7 @@ const router = createBrowserRouter([
     {
         path:'/home',
         element:<HomePage/>
-    }
- 
+    },
+  
 ])
 export default router;
